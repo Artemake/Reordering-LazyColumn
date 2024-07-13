9c7df9c4b6688b48c40852425420a2b81e69da26
E.Artemakis <emmanouil.artemakis@uxma.com>
2024-07-14 00:04:24 +0300

    Initial implementation

--- app/src/main/java/com/manosprojects/reorderablelazycolumn/MainActivity.kt
+++ app/src/main/java/com/manosprojects/reorderablelazycolumn/MainActivity.kt
@@ -3,16 +3,10 @@ package com.manosprojects.reorderablelazycolumn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.manosprojects.reorderablelazycolumn.ui.theme.ReorderableLazyColumnTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MyList()
                 }
             }
         }
