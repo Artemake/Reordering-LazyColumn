d9dc2e6dfe6c6c65547fdb04a8f846cb596cc51a
E.Artemakis <emmanouil.artemakis@uxma.com>
2024-07-13 23:50:19 +0300

    Initial implementation

---
+++ app/src/main/java/com/manosprojects/reorderablelazycolumn/ReorderableLazyColumn.kt
@@ -0,0 +1,60 @@
+package com.manosprojects.reorderablelazycolumn
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/*
+Initial implementation
+ */
+
+@Composable
+fun MyList() {
+    var list1 by remember { mutableStateOf(List(5) { it }) }
+    val list2 by remember { mutableStateOf(List(5) { it + 5 }) }
+
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item {
+            Text(text = "Title 1", fontSize = 30.sp)
+        }
+
+        itemsIndexed(list1) { _, item ->
+            Item(index = item)
+        }
+
+        item {
+            Text(text = "Title 2", fontSize = 30.sp)
+        }
+
+        itemsIndexed(list2) { _, item ->
+            Item(index = item)
+        }
+
+    }
+}
+
+
+@Composable
+private fun Item(modifier: Modifier = Modifier, index: Int) {
+    Card(
+        modifier = modifier
+    ) {
+        Text(
+            "Item $index",
+        )
+    }
+}
