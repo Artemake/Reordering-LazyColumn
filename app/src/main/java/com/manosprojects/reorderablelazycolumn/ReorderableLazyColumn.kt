e3d888e6b58c2f278e8c94f80e15f3c8cebca924
E.Artemakis <emmanouil.artemakis@uxma.com>
2024-07-14 00:36:55 +0300

    Final

--- app/src/main/java/com/manosprojects/reorderablelazycolumn/ReorderableLazyColumn.kt
+++ app/src/main/java/com/manosprojects/reorderablelazycolumn/ReorderableLazyColumn.kt
@@ -1,139 +1,47 @@
 package com.manosprojects.reorderablelazycolumn
 
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
-import kotlinx.coroutines.channels.Channel
 
 /*
-Handling overscroll
+Final
  */
 
 @Composable
 fun MyList() {
     var list1 by remember { mutableStateOf(List(20) { it }) }
     val list2 by remember { mutableStateOf(List(20) { it + 20 }) }
+    val draggableItems by remember {
+        derivedStateOf { list1.size }
+    }
     val stateList = rememberLazyListState()
 
-    var draggingItemIndex: Int? by remember {
-        mutableStateOf(null)
-    }
-
-    var delta: Float by remember {
-        mutableFloatStateOf(0f)
-    }
-
-    var draggingItem: LazyListItemInfo? by remember {
-        mutableStateOf(null)
-    }
-
-    val onMove = { fromIndex: Int, toIndex: Int ->
-        list1 = list1.toMutableList().apply { add(toIndex, removeAt(fromIndex)) }
-    }
-
-    val scrollChannel = Channel<Float>()
-
-    LaunchedEffect(stateList) {
-        while (true) {
-            val diff = scrollChannel.receive()
-            stateList.scrollBy(diff)
-        }
-    }
+    val dragDropState =
+        rememberDragDropState(
+            lazyListState = stateList,
+            draggableItemsNum = draggableItems,
+            onMove = { fromIndex, toIndex ->
+                list1 = list1.toMutableList().apply { add(toIndex, removeAt(fromIndex)) }
+            })
 
     LazyColumn(
-        modifier = Modifier
-            .pointerInput(key1 = stateList) {
-                detectDragGesturesAfterLongPress(
-                    onDragStart = { offset ->
-                        stateList.layoutInfo.visibleItemsInfo
-                            .firstOrNull { item -> offset.y.toInt() in item.offset..(item.offset + item.size) }
-                            ?.also {
-                                (it.contentType as? DraggableItem)?.let { draggableItem ->
-                                    draggingItem = it
-                                    draggingItemIndex = draggableItem.index
-                                }
-                            }
-                    },
-                    onDrag = { change, dragAmount ->
-                        change.consume()
-                        delta += dragAmount.y
-
-                        val currentDraggingItemIndex =
-                            draggingItemIndex ?: return@detectDragGesturesAfterLongPress
-                        val currentDraggingItem =
-                            draggingItem ?: return@detectDragGesturesAfterLongPress
-
-                        val startOffset = currentDraggingItem.offset + delta
-                        val endOffset =
-                            currentDraggingItem.offset + currentDraggingItem.size + delta
-                        val middleOffset = startOffset + (endOffset - startOffset) / 2
-
-                        val targetItem =
-                            stateList.layoutInfo.visibleItemsInfo.find { item ->
-                                middleOffset.toInt() in item.offset..item.offset + item.size &&
-                                        currentDraggingItem.index != item.index &&
-                                        item.contentType is DraggableItem
-                            }
-
-                        if (targetItem != null) {
-                            val targetIndex = (targetItem.contentType as DraggableItem).index
-                            onMove(currentDraggingItemIndex, targetIndex)
-                            draggingItemIndex = targetIndex
-                            delta += currentDraggingItem.offset - targetItem.offset
-                            draggingItem = targetItem
-                        } else {
-                            val startOffsetToTop =
-                                startOffset - stateList.layoutInfo.viewportStartOffset
-                            val endOffsetToBottom =
-                                endOffset - stateList.layoutInfo.viewportEndOffset
-                            val scroll =
-                                when {
-                                    startOffsetToTop < 0 -> startOffsetToTop.coerceAtMost(0f)
-                                    endOffsetToBottom > 0 -> endOffsetToBottom.coerceAtLeast(0f)
-                                    else -> 0f
-                                }
-                            val canScrollDown =
-                                currentDraggingItemIndex != list1.size - 1 && endOffsetToBottom > 0
-                            val canScrollUp = currentDraggingItemIndex != 0 && startOffsetToTop < 0
-                            if (scroll != 0f && (canScrollUp || canScrollDown)) {
-                                scrollChannel.trySend(scroll)
-                            }
-                        }
-                    },
-                    onDragEnd = {
-                        draggingItem = null
-                        draggingItemIndex = null
-                        delta = 0f
-                    },
-                    onDragCancel = {
-                        draggingItem = null
-                        draggingItemIndex = null
-                        delta = 0f
-                    },
-                )
-            },
+        modifier = Modifier.dragContainer(dragDropState),
         state = stateList,
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -142,18 +50,7 @@ fun MyList() {
             Text(text = "Title 1", fontSize = 30.sp)
         }
 
-        itemsIndexed(
-            items = list1,
-            contentType = { index, _ -> DraggableItem(index = index) }) { index, item ->
-            val modifier = if (draggingItemIndex == index) {
-                Modifier
-                    .zIndex(1f)
-                    .graphicsLayer {
-                        translationY = delta
-                    }
-            } else {
-                Modifier
-            }
+        draggableItems(items = list1, dragDropState = dragDropState) { modifier, item ->
             Item(
                 modifier = modifier,
                 index = item,
@@ -185,5 +82,3 @@ private fun Item(modifier: Modifier = Modifier, index: Int) {
         )
     }
 }
-
-data class DraggableItem(val index: Int)
